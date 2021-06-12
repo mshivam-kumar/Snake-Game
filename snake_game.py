1ade8a6706280e46817b1fdd97d80cc1619681cf
Shivam Kumar <svmkumar28@gmail.com>
2021-06-12 10:44:34 +0530

    Add files via upload

--- snake_game.py
+++ snake_game.py
@@ -62,6 +62,7 @@ def welcome():
                     game_loop()
                 if event.key==pygame.K_q:
                     exit_game=True
+                    break
 
         pygame.display.update()
         clock.tick(60)
@@ -86,6 +87,7 @@ def game_loop():
         highscore=f.read()
         
     while not exit_game:
+        prev_head=(snake_x,snake_y)
         if(game_over):
             gameWindow.fill(white)
             text_screen("Game over! press enter to continue",black,200,400)
@@ -105,21 +107,21 @@ def game_loop():
                         exit_game=True
                     if(event.type==pygame.KEYDOWN):
                         if(event.key==pygame.K_RIGHT):
-                            # snake_x+=10
-                            velocity_x=10
+                            # snake_x+=5
+                            velocity_x=5
                             velocity_y=0
                         if(event.key==pygame.K_LEFT):
-                            # snake_x+=10
-                            velocity_x=-10
+                            # snake_x+=5
+                            velocity_x=-5
                             velocity_y=0
                         if(event.key==pygame.K_UP):
-                            velocity_y=-10
+                            velocity_y=-5
                             velocity_x=0
                         if(event.key==pygame.K_DOWN):
-                            velocity_y=10
+                            velocity_y=5
                             velocity_x=0
                         if(event.key==pygame.K_i):
-                            score+=10
+                            score+=7
                             # snk_length+=1
                         if(event.key==pygame.K_q):
                             exit_game=True
@@ -127,7 +129,7 @@ def game_loop():
             snake_x+=velocity_x
             snake_y+=velocity_y
             if(abs(snake_x-food_x)<=30 and abs(snake_y-food_y)<=30):
-                        score+=10
+                        score+=5
                         if(score>int(highscore)):
                             highscore=score
                             with open("highscore.txt",'w') as f:
@@ -136,10 +138,10 @@ def game_loop():
 
                             # highscore=score
 
-                        # print("Score: ",score*10)
+                        # print("Score: ",score*5)
                         food_x=random.randint(20,screen_width/2)
                         food_y=random.randint(20,screen_height/2)
-                        snk_length+=1
+                        snk_length+=12
                         # snk_length+=5
 
             gameWindow.fill(white)
@@ -155,15 +157,17 @@ def game_loop():
             if(len(snk_list)>snk_length):
                 del snk_list[0]
         # pygame.draw.rect(gameWindow,black,[snake_x,snake_y,snake_size,snake_size])
-            # if(head in snk_list[:-1]):
-                # game_over=True
+            if(head in snk_list[:-1]):
+                game_over=True
             # for x in snk_list[:-1]:
                 # if x == head:
                     # game_over = True
                     # break
+                
             # for p in snk_list[:-1]:
                 # if(p[0]==head[0] and p[1]==head[1]):
-                    # game_over=True
+                    # if(prev_head[0]!=head[0] or prev_head[1]!=head[1]):
+                        # game_over=True
                     # break
             if snake_x<0 or snake_x>screen_width or snake_y<0 or snake_y>screen_height:
                 pygame.mixer.music.load("gameover.mp3")
@@ -175,8 +179,6 @@ def game_loop():
             plot_snake(gameWindow,black,snk_list,snake_size)
         pygame.display.update()
         clock.tick(fps)
-    print(head)
     pygame.quit()
     quit()
-# game_loop()
 welcome()
